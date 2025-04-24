23e4eef9db4971023af7e276e0c58a3622c12990
kakaxi <oday0311@hotmail.com>
2025-04-24 10:00:42 +0800

    update..

--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -7,7 +7,7 @@ use std::sync::atomic::Ordering::Relaxed;
 mod check_tfhe_docs_are_tested;
 mod format_latex_doc;
 mod utils;
-
+use log::info;
 // -------------------------------------------------------------------------------------------------
 // CONSTANTS
 // -------------------------------------------------------------------------------------------------
@@ -35,6 +35,11 @@ fn main() -> Result<(), std::io::Error> {
             Arg::new("dry-run")
                 .long("dry-run")
                 .help("Do not execute the commands"),
+        )
+        .arg(
+            Arg::new("test")
+                .long("test")
+                .help("a test command"),
         )
         .subcommand(Command::new(FORMAT_LATEX_DOC).about("Escape underscores in latex equations"))
         .subcommand(
@@ -72,5 +77,82 @@ fn main() -> Result<(), std::io::Error> {
         check_tfhe_docs_are_tested::check_tfhe_docs_are_tested()?;
     }
 
+
+
+    if matches.contains_id("test") {
+        info!("Test command executed");
+    }
+
+    test_process();
+    Ok(())
+}
+
+
+#[cfg(test)]
+fn main_test() {
+    // This is a test function to run the main function in a test context
+    // It is not used in the actual code, but can be used for testing purposes
+    info!("Test function executed");
+    test_process();
+}
+
+// -------------------------------------------------------------------------------------------------
+use tfhe::prelude::*;
+use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint32, FheUint8};
+
+fn test_process() -> Result<(), Box<dyn std::error::Error>> {
+    info!("start process init...");
+    // Basic configuration to use homomorphic integers
+    let config = ConfigBuilder::default().build();
+
+    // Key generation
+    let (client_key, server_keys) = generate_keys(config);
+
+    let clear_a = 1344u32;
+    let clear_b = 5u32;
+    let clear_c = 7u8;
+
+    // Encrypting the input data using the (private) client_key
+    // FheUint32: Encrypted equivalent to u32
+    let mut encrypted_a = FheUint32::try_encrypt(clear_a, &client_key)?;
+    let encrypted_b = FheUint32::try_encrypt(clear_b, &client_key)?;
+
+    // FheUint8: Encrypted equivalent to u8
+    let encrypted_c = FheUint8::try_encrypt(clear_c, &client_key)?;
+
+    // On the server side:
+    set_server_key(server_keys);
+
+    info!("set process server key...");
+
+
+    // Clear equivalent computations: 1344 * 5 = 6720
+    let encrypted_res_mul = &encrypted_a * &encrypted_b;
+    info!("set process sClear equivalent computations: 1344 * 5 = 6720...");
+
+    // Clear equivalent computations: 6720 >> 5 = 210
+    encrypted_a = &encrypted_res_mul >> &encrypted_b;
+    info!("set process Clear equivalent computations: 6720 >> 5 = 210");
+
+    // Clear equivalent computations: let casted_a = a as u8;
+    let casted_a: FheUint8 = encrypted_a.cast_into();
+    info!("set process Clear equivalent computations: let casted_a = a as u8;");
+
+    // Clear equivalent computations: min(210, 7) = 7
+    let encrypted_res_min = &casted_a.min(&encrypted_c);
+    info!("Clear equivalent computations: min(210, 7) = 7");
+
+
+    // Operation between clear and encrypted data:
+    // Clear equivalent computations: 7 & 1 = 1
+    let encrypted_res = encrypted_res_min & 1_u8;
+
+    info!("encrpted res...");
+
+
+    // Decrypting on the client side:
+    let clear_res: u8 = encrypted_res.decrypt(&client_key);
+    assert_eq!(clear_res, 1_u8);
+    info!("test process executed successfully");
     Ok(())
 }
